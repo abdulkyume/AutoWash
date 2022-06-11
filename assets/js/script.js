bf89f6986f8645c740fe0b7be86057e21795d326
abdulkyume <abbirtak@gmail.com>
2022-06-11 16:44:20 +0600

    choose plan

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,23 @@ window.onscroll = function () {
         myNav.classList.remove("responsivenav");
     }
 };
+
+const counters = document.querySelectorAll(".count");
+const speed = 50;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = parseInt(+counter.getAttribute("data-target"));
+    const count = parseInt(+counter.innerText);
+    const increment = Math.trunc(target / speed);
+    console.log(increment);
+
+    if (count < target) {
+      counter.innerText = count + increment;
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+  updateCount();
+});
